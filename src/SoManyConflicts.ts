bda733d2f2537e99ce81bccbc8ee004ee0410b1e
Symbolk <symbolk@163.com>
2021-05-10 16:44:25 +0800

    refa: use async/await to replace then.

--- src/SoManyConflicts.ts
+++ src/SoManyConflicts.ts
@@ -5,34 +5,28 @@ import { Parser } from './Parser'
 import { ISection } from './ISection'
 
 export class SoManyConflicts {
-  public static scanAllConflicts(workspace: string): void {
+  public static async scanAllConflicts(workspace: string): Promise<void> {
     let message: string = ''
     // get all files in conflict state in the opened workspace
     // case1: git repo
-    let filePaths: string[]
-    SoManyConflicts.getConflictingFiles(workspace)
-      .then((res) => {
-        if (res) {
-          for (const path of res) {
-            // scan and parse all conflict blocks
-            let content = readFileSync(workspace + '/' + path, 'utf-8')
-            const sections: ISection[] = Parser.parse(content)
-            console.log()
-          }
-          // build partial order between conflicts (identifier or LSP)
-          // must consider the conflict blocks and also the context of it to work better
-
-
+    try {
+      let filePaths: string[] = await this.getConflictingFiles(workspace)
+      if (filePaths) {
+        for (const path of filePaths) {
+          // scan and parse all conflict blocks
+          let content = readFileSync(workspace + '/' + path, 'utf-8')
+          const sections: ISection[] = Parser.parse(content)
+          console.log()
         }
-      })
-      .catch((err) => {
-        vscode.window.showInformationMessage(err)
-      })
+      }
+    } catch (err) {
+      vscode.window.showInformationMessage(err)
+    }
   }
 
   // private static parseConflicts(): {}
 
-  public static getConflictingFiles(path: string): Promise<string[]> {
+  public static async getConflictingFiles(path: string): Promise<string[]> {
     const git: SimpleGit = simpleGit(path)
     // const status: StatusResult = await git.status();
 
